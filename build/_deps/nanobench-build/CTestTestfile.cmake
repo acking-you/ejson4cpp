db15dfc14b3a5fba6b549173e51a48c91104b3c1
ackingliu <ackingliu@tencent.com>
2023-11-30 19:13:14 +0800

    fix: unnecessary build

--- build/_deps/nanobench-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/acking-liu/CLionProjects/ejson4cpp/build/_deps/nanobench-src
-# Build directory: /Users/acking-liu/CLionProjects/ejson4cpp/build/_deps/nanobench-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CMakeFiles/benchtest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for benchtest.
-# This may be replaced when dependencies are built.
